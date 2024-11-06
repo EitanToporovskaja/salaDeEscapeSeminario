5339e8df9c5fc6b0f94b90447a350bc3416b05c1
eitan <mail@mail.com>
2024-11-06 10:22:42 -0300

    Listo ahora si

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
-function celebrate() {
-    
+function openModal() {
+    const modal = document.getElementById('celebrationModal');
+    modal.style.display = 'flex'; 
+
     confetti({
-      particleCount: 200,
-      spread: 60,   
-      origin: { x: 0.5, y: 0.5 },
-      colors: ['#ff0000', '#ff7300', '#fffb00', '#00ff4f', '#00d4ff', '#8200ff'],
-    });
-  
-    const messageBox = document.querySelector('.message-box');
-    messageBox.style.transform = 'scale(1.1)';
-    setTimeout(() => {
-      messageBox.style.transform = 'scale(1)';
-    }, 500);
+        particleCount: 200, 
+        spread: 60,      
+        origin: { x: 0.5, y: 0.5 },
+        colors: ['#ff0000', '#ff7300', '#fffb00', '#00ff4f', '#00d4ff', '#8200ff'],
+      });
   }
+  
+  /*
+  function closeModal() {
+    const modal = document.getElementById('celebrationModal');
+    modal.style.display = 'none';
+  
+    // 
+    confetti({
+      particleCount: 200,  
+      spread: 60,          
+      origin: { x: 0.5, y: 0.5 },
+      colors: ['#ff0000', '#ff7300', '#fffb00', '#00ff4f', '#00d4ff', '#8200ff'], 
+    });
+  }*/
   
